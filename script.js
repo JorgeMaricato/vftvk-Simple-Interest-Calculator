9c1646479ee60af66f475514cc146c012c8f1d29
JorgeMaricato <jorgemaricato@gmail.com>
2021-10-11 16:37:44 -0300

    Product after main instructions
    Updated index.html, script.js and style.css as first version after implement all the requisites

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = parseInt(document.getElementById("principal").value) || 0;
+
+    if (principal <= 0 ) {
+        alert("Insert a positive \"Amount\", please!");
+        document.getElementById("principal").focus();
+    }
+    else {
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate / 100;
+        var year = new Date().getFullYear()+parseInt(years);
     
+        var amount = parseFloat(interest);
+        document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal+"\<\/mark\>,\<br\>at an interest rate of \<mark\>"+rate+"%\<\/mark\>\<br\>You will receive an amount of \<mark\>"+amount+"\<\/mark\>,\<br\>in the year \<mark\>"+year+"\<\/mark\>\<br\>";
+    }
 }
-        
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+}
